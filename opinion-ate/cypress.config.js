f193ce05168620046238b1619ce6061e662a15dd
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-09-03 23:12:17 -0300

    chore: set up test action

--- opinion-ate/cypress.config.js
+++ opinion-ate/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://dev:3000',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
     supportFile: false
   }
 })
