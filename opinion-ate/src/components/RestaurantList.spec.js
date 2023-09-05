f0d6527db90ff198996ae025e0b77c2e4e7f8ede
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-09-05 16:08:37 -0300

    refactor(test): remove duplication

--- opinion-ate/src/components/RestaurantList.spec.js
+++ opinion-ate/src/components/RestaurantList.spec.js
@@ -3,9 +3,15 @@ import { render, screen } from '@testing-library/react'
 import RestaurantList from './RestaurantList'
 
 describe('<RestaurantList />', () => {
-  it('should load restaurants on first render', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants')
-    const restaurants = []
+  const restaurants = [
+    { id: 1, name: 'Sushi Place' },
+    { id: 2, name: 'Pizza Place' },
+  ]
+
+  let loadRestaurants
+
+  function renderComponent() {
+    loadRestaurants = jest.fn().mockName('loadRestaurants')
 
     render(
       <RestaurantList
@@ -13,18 +19,16 @@ describe('<RestaurantList />', () => {
         restaurants={restaurants}
       />
     )
+  }
+
+  it('should load restaurants on first render', () => {
+    renderComponent()
 
     expect(loadRestaurants).toHaveBeenCalled()
   })
 
   it('should display the restaurants', () => {
-    const noop = () => {}
-    const restaurants = [
-      { id: 1, name: 'Sushi Place' },
-      { id: 2, name: 'Pizza Place' },
-    ]
-
-    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants}/>)
+    renderComponent()
 
     expect(screen.getByText('Sushi Place')).toBeInTheDocument()
     expect(screen.getByText('Pizza Place')).toBeInTheDocument()
