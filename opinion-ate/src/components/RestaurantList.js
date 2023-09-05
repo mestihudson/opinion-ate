d2236aebd83c382deedaf4c2edbf84d18baa0bba
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-09-05 14:43:28 -0300

    feat: load restaurants on first render

--- opinion-ate/src/components/RestaurantList.js
+++ opinion-ate/src/components/RestaurantList.js
@@ -1,4 +1,10 @@
-export default function RestaurantList() {
+import { useEffect } from 'react'
+
+export default function RestaurantList({ loadRestaurants }) {
+  useEffect(() => {
+    loadRestaurants()
+  }, [loadRestaurants])
+
   return (
     <div>
       <h1>RestaurantList</h1>
