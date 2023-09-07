3389ccc5c854d8854c968faeb2db25ec0975e28e
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-09-07 07:54:31 -0300

    refactor: style App component with @mui parts

--- opinion-ate/src/App.js
+++ opinion-ate/src/App.js
@@ -1,12 +1,36 @@
 import { Provider } from 'react-redux'
+import { red } from '@mui/material/colors'
+import { createTheme } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
+import AppBar from '@mui/material/AppBar'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
 
 import store from './store'
 import RestaurantScreen from './components/RestaurantScreen'
 
+const theme = createTheme({
+  palette: {
+    primary: red,
+  },
+})
+
 export default function App() {
   return (
     <Provider store={store}>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Opinion Ate</Typography>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <RestaurantScreen />
+        </Container>
+      </ThemeProvider>
     </Provider>
   )
 }
