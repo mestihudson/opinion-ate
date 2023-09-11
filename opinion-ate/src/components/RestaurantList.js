4011a393341984e0cbb415f553ca4d9f69662deb
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-09-11 12:50:54 -0300

    feat: add loading state to props mapping on restaurant list component

--- opinion-ate/src/components/RestaurantList.js
+++ opinion-ate/src/components/RestaurantList.js
@@ -28,6 +28,7 @@ export function RestaurantList({ loadRestaurants, restaurants, loading, }) {
 
 const mapStateToProps = (state) => ({
   restaurants: state.restaurants.records,
+  loading: state.restaurants.loading,
 })
 
 const mapDispatchToProps = { loadRestaurants }
