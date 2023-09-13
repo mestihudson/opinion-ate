31ffe86c6daeb365f2352a486017d871dcd46033
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-09-12 23:32:28 -0300

    test: should call createRestaurant with the name (when filled in)

---
+++ opinion-ate/src/components/NewRestaurantForm.spec.js
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { NewRestaurantForm } from './NewRestaurantForm'
+
+describe('<NewRestaurantForm />', () => {
+  const restaurantName = 'Sushi Place'
+  let user
+  let createRestaurant
+
+  function renderComponent() {
+    user = userEvent.setup()
+    createRestaurant = jest.fn().mockName('createRestaurant')
+
+    render(<NewRestaurantForm createRestaurant={createRestaurant} />)
+  }
+
+  describe('when filled in', () => {
+    async function fillInForm() {
+      renderComponent()
+      await user.type(
+        screen.getByPlaceholderText('Add Restaurant'),
+        restaurantName,
+      )
+      await user.click(screen.getByText('Add'))
+    }
+
+    it('should call createRestaurant with the name', async () => {
+      await fillInForm()
+      expect(createRestaurant).toHaveBeenCalledWith(restaurantName)
+    })
+  })
+})
