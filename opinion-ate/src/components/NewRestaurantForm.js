b0d5cbd6afa60055b1b9c3cd81638ce080eb60c2
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-09-12 23:13:24 -0300

    feat: add add restaurant button to new restaurant form component structure

--- opinion-ate/src/components/NewRestaurantForm.js
+++ opinion-ate/src/components/NewRestaurantForm.js
@@ -1,9 +1,11 @@
+import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 
 export function NewRestaurantForm() {
   return (
     <form>
       <TextField placeholder="Add Restaurant" variant="filled" fullWidth />
+      <Button variant="contained" color="primary">Add</Button>
     </form>
   )
 }
