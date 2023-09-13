7a153a7d5f0893703127a207fbc614c73cec793b
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-09-13 01:05:53 -0300

    feat: display a server error (when the store action rejects)

--- opinion-ate/src/components/NewRestaurantForm.js
+++ opinion-ate/src/components/NewRestaurantForm.js
@@ -14,7 +14,9 @@ export function NewRestaurantForm({ createRestaurant }) {
     e.preventDefault()
     if (name) {
       setValidationError(false)
-      await createRestaurant(name)
+      try {
+        await createRestaurant(name)
+      } catch {}
     } else {
       setValidationError(true)
     }
@@ -23,6 +25,7 @@ export function NewRestaurantForm({ createRestaurant }) {
 
   return (
     <form onSubmit={handleSubmit}>
+      <Alert severity="error">The restaurant could not be saved. Please try again.</Alert>
       { validationError && <Alert severity="error">Name is required</Alert> }
       <TextField
         value={name}
