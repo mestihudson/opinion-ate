0464f3a32d0c940b6ef58e247c1af085f1a4ff69
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-09-13 00:26:31 -0300

    refactor(test): extract literal to reusable field

--- opinion-ate/src/components/NewRestaurantForm.spec.js
+++ opinion-ate/src/components/NewRestaurantForm.spec.js
@@ -5,6 +5,7 @@ import { NewRestaurantForm } from './NewRestaurantForm'
 
 describe('<NewRestaurantForm />', () => {
   const restaurantName = 'Sushi Place'
+  const requiredError = 'Name is required'
   let user
   let createRestaurant
 
@@ -46,7 +47,7 @@ describe('<NewRestaurantForm />', () => {
     it('should display a validation error', async () => {
       await submitEmptyForm()
 
-      expect(screen.getByText('Name is required')).toBeInTheDocument()
+      expect(screen.getByText(requiredError)).toBeInTheDocument()
     })
   })
 })
